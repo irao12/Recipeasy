947f615dcb2c074d4eb6f025d4d9dd2e6df44164
irao12 <ivanrao@outlook.com>
2023-01-12 12:07:58 -0500

    added files for the server

--- api/models/user.js
+++ api/models/user.js
@@ -4,39 +4,45 @@ const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
 	class User extends Model {}
 
-	User.init({
-		firstName: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		lastName: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		email: {
-			type: DataTypes.STRING,
-			unique: true,
-			allowNull: false,
-			validate: {
-				isEmail: true,
+	User.init(
+		{
+			firstName: {
+				type: DataTypes.STRING,
+				allowNull: false,
 			},
-		},
-		passwordHash: {
-			type: DataTypes.STRING,
-		},
-		password: {
-			type: DataTypes.VIRTUAL,
-			validate: {
-				isLongEnough: (val) => {
-					if (val.length < 6) {
-						throw new Error(
-							"Password must have at least 7 characters"
-						);
-					}
+			lastName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: DataTypes.STRING,
+				unique: true,
+				allowNull: false,
+				validate: {
+					isEmail: true,
+				},
+			},
+			passwordHash: {
+				type: DataTypes.STRING,
+			},
+			password: {
+				type: DataTypes.VIRTUAL,
+				validate: {
+					isLongEnough: (val) => {
+						if (val.length < 6) {
+							throw new Error(
+								"Password must have at least 7 characters"
+							);
+						}
+					},
 				},
 			},
 		},
-	});
+		{
+			sequelize,
+			modelName: "user",
+		}
+	);
 
 	User.associate = (models) => {
 		// associations
