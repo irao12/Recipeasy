76255600070d0e7bc1781f9a224d4e7160a792b4
irao12 <ivanrao@outlook.com>
2023-01-16 21:25:27 -0500

    changed logout button to redirect to the login page after

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,15 +2,21 @@ import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Spoon from "../assets/icons/silverware-spoon.svg";
 import LogOutIcon from "../assets/icons/logout.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
 	const auth = useContext(AuthContext);
+	const navigate = useNavigate();
 
 	return (
 		<nav className="navbar">
-			<div className="logo">
+			<div
+				className="logo"
+				onClick={() => {
+					navigate("/");
+				}}
+			>
 				<img src={Spoon} alt="spoon" className="logo-spoon" />
 				<h1 className="logo-text">Recipeasy</h1>
 			</div>
@@ -39,6 +45,7 @@ export default function Navbar() {
 							title="Log Out"
 							onClick={() => {
 								auth.signout();
+								navigate("/login");
 							}}
 						>
 							<img
