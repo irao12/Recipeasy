de2777b6c90cbe786ab34ba5cf5e86966adcf5de
irao12 <ivanrao@outlook.com>
2023-01-17 23:08:17 -0500

    made changes to find recipes page

--- client/src/pages/FindRecipesPage.js
+++ client/src/pages/FindRecipesPage.js
@@ -4,8 +4,12 @@ export default function FindRecipesPage() {
 	return (
 		<div className="find-recipes-page">
 			<div className="find-recipes-content">
-				<div className="find-recipes-results"></div>
-				<div className="ingredients"></div>
+				<div className="find-recipes-results">
+
+        </div>
+				<div className="ingredients">
+
+        </div>
 			</div>
 		</div>
 	);
