a4c53d4887566dbcf3e26f968727e50d9a557b08
gnauh <evanacademics@gmail.com>
2023-02-19 17:36:03 -0500

    Added ingredient searching

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import RecipeBox from "../components/RecipeBox";
 import "./FavoritesPage.css";
@@ -7,33 +7,47 @@ export default function FavoritesPage() {
   const auth = useContext(AuthContext);
   const [favorites, setFavorites] = useState([]);
   const [sortBy, setSortBy] = useState("Newest");
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientInput, setIngredientInput] = useState("");
 
-  const getFavorites = async () => {
-    try {
-      const response = await fetch(`/api/favorite/all`);
-      const favorites = await response.json();
-      console.log("Init Fav: ", favorites);
-      setFavorites(favorites);
-    } catch (err) {
-      console.log(err);
+  const handleInputChange = (event) => {
+    setIngredientInput(event.target.value);
+  };
+
+  const handleEnterKey = (event) => {
+    if (event.key === "Enter") {
+      addIngredient(ingredientInput);
+      setIngredientInput("");
     }
   };
 
-  useEffect(() => {
-    getFavorites();
-  }, [auth.user.id]);
-
-  const mapFavoritesToRecipe = (favorite) => {
-    return {
-      title: favorite.title,
-      image: favorite.imageURL,
-      missedIngredientCount: 0,
-      sourceUrl: favorite.recipeURL,
-      id: favorite.recipeID,
-      isFavorited: favorite.isFavorited,
-      ingredients: favorite.ingredients ? favorite.ingredients : [],
-    };
+  const addIngredient = (ingredient) => {
+    if (!ingredient) {
+      return;
+    }
+    if (ingredients.includes(ingredient)) {
+      console.log(`${ingredient} is already in the list`);
+      return;
+    }
+    const newIngredients = [...ingredients, ingredient];
+    setIngredients(newIngredients);
   };
+
+  const deleteIngredient = (index) => {
+    const newIngredients = [...ingredients];
+    newIngredients.splice(index, 1);
+    setIngredients(newIngredients);
+  };
+
+  const mapFavoriteToRecipe = (favorite) => ({
+    title: favorite.title,
+    image: favorite.imageURL,
+    missedIngredientCount: 0,
+    sourceUrl: favorite.recipeURL,
+    id: favorite.recipeID,
+    isFavorited: favorite.isFavorited,
+    ingredients: favorite.ingredients || [],
+  });
 
   const sortFavoritesByDate = (favorites) => {
     if (!Array.isArray(favorites)) {
@@ -52,42 +66,113 @@ export default function FavoritesPage() {
     return favorites;
   };
 
-  const sortedFavorites = (() => {
+  const filteredFavorites = favorites.filter((favorite) =>
+    ingredients.every((ingredient) =>
+      favorite.ingredients.some((favIngredient) =>
+        favIngredient.includes(ingredient)
+      )
+    )
+  );
+
+  const sortedFavorites = useMemo(() => {
+    console.log("Filtered Favorites: ", filteredFavorites); // add this line
     switch (sortBy) {
       case "Newest":
-        return sortFavoritesByDate(favorites);
+        return sortFavoritesByDate(filteredFavorites);
       case "Oldest":
-        return sortFavoritesByDate(favorites).reverse();
+        return sortFavoritesByDate(filteredFavorites).reverse();
       default:
-        return favorites;
+        return filteredFavorites;
     }
-  })();
+  }, [filteredFavorites, sortBy]);
 
   const handleSortChange = (event) => {
     setSortBy(event.target.value);
   };
 
+  const getFavorites = async () => {
+    try {
+      const response = await fetch(`/api/favorite/all`);
+      const favorites = await response.json();
+      console.log("Init Fav: ", favorites);
+      setFavorites(favorites);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, [auth.user.id]);
+
+
+  
   return (
-    <div className="favorites-page">
-      <div className="favorites-content">
-        <div className="favorites-results">
-          <h1>Favorites</h1>
-          <div className="sort-by-container">
-            <label htmlFor="sort-by-select">Sort By:</label>
-            <select id="sort-by-select" value={sortBy} onChange={handleSortChange}>
-              <option value="Newest">Newest</option>
-              <option value="Oldest">Oldest</option>
-              <option value="Ingredients">Number of Ingredients (WIP)</option>
-              <option value="Search">Search Ingredients (WIP)</option>
-            </select>
-          </div>
-          <div className="recipes-container">
-            {sortedFavorites.map((favorite) => (
-              <RecipeBox key={favorite.id} recipe={mapFavoritesToRecipe(favorite)} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+	<div className="favorites-page">
+	  <div className="favorites-content">
+		<div className="favorites-results">
+		  <h1>Favorites</h1>
+		  <div className="recipes-container">
+			{sortedFavorites.map((favorite) => (
+			  <RecipeBox key={favorite.id} recipe={mapFavoriteToRecipe(favorite)} />
+			))}
+		  </div>
+		</div>
+  
+		<div className="ingredients-container">
+		  <div className="ingredients-content">
+			<h1>Favorites Filter</h1>
+			<div className="add-ingredient-section">
+			  <label htmlFor="sort-by-select">Sort:</label>
+			  <div className="sort-by-container">
+				<select id="sort-by-select" value={sortBy} onChange={handleSortChange}>
+				  <option value="Newest">Newest</option>
+				  <option value="Oldest">Oldest</option>
+				</select>
+			  </div>
+			</div>
+  
+			<label htmlFor="add-ingredient-field">Add Ingredient</label>
+			<div className="add-ingredient-section">
+			  <input
+				id="add-ingredient-field"
+				type="text"
+				value={ingredientInput}
+				onChange={handleInputChange}
+				onKeyDown={handleEnterKey}
+			  />
+			  <button
+				className="add-ingredient-button"
+				onClick={() => {
+				  addIngredient(ingredientInput);
+				  setIngredientInput("");
+				}}
+			  >
+				+
+			  </button>
+			</div>
+			<ul className="ingredients-list">
+			  {ingredients.map((ingredient, index) => {
+				return (
+				  <li key={index} className="ingredient">
+					<p onClick={() => { deleteIngredient(index); }}>
+					  - {ingredient}
+					</p>
+				  </li>
+				);
+			  })}
+			</ul>
+  
+			<button
+			  className="find-recipes-button"
+			  type="button"
+			  onClick={getFavorites}
+			>
+			  Favorites
+			</button>
+		  </div>
+		</div>
+	  </div>
+	</div>
   );
-}
+}  
