d70976ac818f618675c4ad2122ae934bff651468
gnauh <evanacademics@gmail.com>
2023-02-19 18:14:43 -0500

    added outline & css for history page

--- client/src/pages/HistoryPage.js
+++ client/src/pages/HistoryPage.js
@@ -1,27 +1,46 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import "./HistoryPage.css";
+
+
 export default function HistoryPage() {
-	const auth = useContext(AuthContext);
-	const [history, setHistory] = useState([]);
+  const auth = useContext(AuthContext);
+  const [history, setHistory] = useState([]);
 
-	const getHistory = () => {
-		fetch(`/api/history/`).then((response) => {
-			if (!response.ok) {
-				throw new Error("failed to get history");
-			}
-			response.json().then((history) => setHistory(history));
-		});
-	};
+  const getHistory = () => {
+    fetch(`/api/history/`).then((response) => {
+      if (!response.ok) {
+        throw new Error("failed to get history");
+      }
+      response.json().then((history) => setHistory(history));
+    });
+  };
 
-	return (
-		<div className="history-page">
-			<div className="history-content">History</div>
-			<div className="">
+  // search filter.
+  
+  // div updater (update history)
+
+  // display info functions
+
+
+
+
+
+  useEffect(() => {
+    console.log(history); 
+  }, [history]);
+
+  return (
+    <div className="history-page">
+		<div className = "history-content">
+			<h1>History</h1>
+			<div className="history-box">
 				<button onClick={getHistory}>GET HISTORY</button>
 				{history.map((his) => (
-					<div>{his.title}</div>
+				<div key={his.id}>{his.title}</div>
 				))}
 			</div>
 		</div>
-	);
+    </div>
+  );
 }
