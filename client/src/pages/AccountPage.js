59dfecf2098e0a50951cc1d9f08a595c70775772
irao12 <ivanrao@outlook.com>
2023-02-26 18:17:41 -0500

    added a welcome message in the account page

--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import "./AccountPage.css";
 
 export default function AccountPage() {
+	const auth = useContext(AuthContext);
 	const navigate = useNavigate();
 	return (
 		<div className="account-page">
 			<div className="account-content">
+				<h1 className="welcome">Hi {auth.user.firstName}!</h1>
 				<button
 					className="account-page-button"
 					onClick={() => {
